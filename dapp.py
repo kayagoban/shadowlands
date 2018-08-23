736a2e876f46339bc6c8ad14d485eb2ac7b96441
C. Thomas <cthomas@railjumper.com>
2018-08-23 14:02:45 -0500

    docs

--- dapp.py
+++ dapp.py
@@ -24,6 +24,9 @@ def register_w3_on_contracts():
 # import pdb; pdb.set_trace()
 
 
+# Sets the resolver on your name record.  No idea why we have to do this - there should 
+# only be one resolver per ENS registrar.  But who am I to question the gods?
+#
 # Both of these invocations have the same precise effect.  the .eth domain will be added
 # if it is not already in the string.
 #
@@ -34,11 +37,21 @@ def register_ens_resolver(name):
         EnsRegistry.set_resolver(name) 
     )
 
+# Sets the address that your name resolves to.  Will fail if your credstick does not
+# own the name.
+#
+# set_ens_resolver_address('ceilingcat', '0xb75D1e62b10E4ba91315C4aA3fACc536f8A922F5')
+# set_ens_resolver_address('ceilingcat.eth', '0xb75D1e62b10E4ba91315C4aA3fACc536f8A922F5')
 def set_ens_resolver_address(name, address_target):
     return push(
         EnsResolver.set_address(name, address_target)
     )
-   
+
+# Sets the reverse record for the name given to the address of your credstick,
+# if indeed you own the name.  This is how you set your nick.
+# 
+# set_ens_reverse_lookup('ceilingcat')
+# set_ens_reverse_lookup('ceilingcat.eth')
 def set_ens_reverse_lookup(name):
     return push(
         EnsReverseResolver.set_name(name)
