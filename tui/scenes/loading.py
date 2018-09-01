deadbeb2d617aae3a327e10e747f932ac4791903
C. Thomas <cthomas@railjumper.com>
2018-09-01 18:02:09 -0500

    Consolidated dynamic renderers into one file.

--- tui/scenes/loading.py
+++ tui/scenes/loading.py
@@ -2,8 +2,8 @@ from asciimatics.renderers import StaticRenderer, FigletText
 from asciimatics.scene import Scene
 from tui.effects.materialize import Materialize
 from tui.effects.cursor import LoadingScreenCursor
-from tui.effects.blockstatus import BlockStatusCursor
-from tui.effects.networkstatus import NetworkStatusCursor
+from tui.effects.dynamic_cursor import DynamicSourceCursor
+from tui.renderers import BlockStatusRenderer, NetworkStatusRenderer
 from tui.effects.credstick_watcher import CredstickWatcher
 
 
@@ -15,8 +15,10 @@ Or just keep hitting the enter button.  Have fun with that.'''
 class LoadingScene(Scene):
     def __init__(self, screen, _name, interface):
         effects = [
-            BlockStatusCursor(screen, interface.node, 0, 0, speed=4, no_blink=True),
-            NetworkStatusCursor(screen, interface.node, 60, 0, speed=4, no_blink=True),
+
+            DynamicSourceCursor(screen, BlockStatusRenderer(interface.node), 0, 0, speed=4, no_blink=True),
+            #BlockStatusCursor(screen, interface.node, 0, 0, speed=4, no_blink=True),
+            DynamicSourceCursor(screen, NetworkStatusRenderer(interface.node), 60, 0, speed=4, no_blink=True),
             Materialize(screen, FigletText('Shadowlands', 'slant'), 0, 2, signal_acceleration_factor=1.1, start_frame=15),
             Materialize(screen, StaticRenderer([ 'p u b l i c    t e r m i n a l\t\t\tv0 . 0 1']), 10, 9, signal_acceleration_factor=1.0005,start_frame=35),
             LoadingScreenCursor(screen, StaticRenderer([PROMPT]), 0, 13, start_frame=75, speed=4, no_blink=False, thread=True),
--- tui/scenes/main.py
+++ tui/scenes/main.py
@@ -1,8 +1,8 @@
 from asciimatics.renderers import StaticRenderer
 from asciimatics.scene import Scene
 from tui.effects.materialize import Materialize
-from tui.effects.blockstatus import BlockStatusCursor
-from tui.effects.networkstatus import NetworkStatusCursor
+from tui.effects.dynamic_cursor import DynamicSourceCursor
+from tui.renderers import BlockStatusRenderer, NetworkStatusRenderer
 
 
 #from tui.effects.cursor import LoadingScreenCursor
@@ -10,8 +10,9 @@ from tui.effects.networkstatus import NetworkStatusCursor
 class MainScene(Scene):
     def __init__(self, screen, _name, node):
         effects = [
-            BlockStatusCursor(screen, node, 0, 0, speed=4, no_blink=True),
-            NetworkStatusCursor(screen, node, 60, 0, speed=4, no_blink=True)
+            DynamicSourceCursor(screen, BlockStatusRenderer(node), 0, 0, speed=4, no_blink=True),
+            DynamicSourceCursor(screen, NetworkStatusRenderer(node), 60, 0, speed=4, no_blink=True)
+ 
             #Materialize(screen, FigletText('Shadowlands', 'slant'), 0, 2, signal_acceleration_factor=1.1, start_frame=15),
             #Materialize(screen, StaticRenderer([ 'p u b l i c    t e r m i n a l\t\t\tv0 . 0 1']), 10, 9, signal_acceleration_factor=1.0005,start_frame=35),
             #LoadingScreenCursor(screen, StaticRenderer([PROMPT]), 0, 13, start_frame=75, speed=4, no_blink=False, thread=True)
