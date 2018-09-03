1316fb4d5bee5748d7f14a23d2ee10646b4f64f9
C. Thomas <cthomas@railjumper.com>
2018-09-03 16:29:06 -0500

    My label does have a name.  His name was Robert Paulson.

--- asciimatics/widgets.py
+++ asciimatics/widgets.py
@@ -1657,7 +1657,7 @@ class Label(Widget):
     A text label.
     """
 
-    def __init__(self, label, height=1, align="<"):
+    def __init__(self, label, height=1, align="<", name=None):
         """
         :param label: The text to be displayed for the Label.
         :param height: Optional height for the label.  Defaults to 1 line.
@@ -1666,7 +1666,7 @@ class Label(Widget):
 
         """
         # Labels have no value and so should have no name for look-ups either.
-        super(Label, self).__init__(None, tab_stop=False)
+        super(Label, self).__init__(name, tab_stop=False)
         # Although this is a label, we don't want it to contribute to the layout
         # tab calculations, so leave internal `_label` value as None.
         self._text = label
