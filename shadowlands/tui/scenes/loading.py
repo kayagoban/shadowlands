8ba5138bef48b61fec0d110c8cd41c0f4d9fcb5d
C. Thomas <cthomas@railjumper.com>
2018-09-17 14:47:50 -0500

    Add Ropsten ENS address &  small cosmetic stuff

--- shadowlands/tui/scenes/loading.py
+++ shadowlands/tui/scenes/loading.py
@@ -17,8 +17,8 @@ class LoadingScene(Scene):
         effects = [
 
             DynamicSourceCursor(screen, BlockStatusRenderer(interface.node), 0, 0, speed=4, no_blink=True),
-            Materialize(screen, StaticRenderer(['${7,1}N${2,2}etwork:' ]), 45, 0, signal_acceleration_factor=2),
-            DynamicSourceCursor(screen, NetworkStatusRenderer(interface.node), 55, 0, speed=4),
+            Materialize(screen, StaticRenderer(['${7,1}N${2,2}etwork:' ]), 41, 0, signal_acceleration_factor=2),
+            DynamicSourceCursor(screen, NetworkStatusRenderer(interface.node), 51, 0, speed=4),
             Materialize(screen, FigletText('Shadowlands', 'slant'), 0, 2, signal_acceleration_factor=1.1, start_frame=15),
             Materialize(screen, StaticRenderer([ 'p u b l i c    t e r m i n a l\t\t\tv0 . 0 1']), 10, 9, signal_acceleration_factor=1.0005,start_frame=35),
 
