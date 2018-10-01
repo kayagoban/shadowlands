be4b3957392a41762402bbb1b03e7ca6439392c7
C. Thomas <cthomas@railjumper.com>
2018-10-01 14:18:58 -0500

    Reform Dapps menu

--- shadowlands/dapp_browser.py
+++ shadowlands/dapp_browser.py
@@ -9,27 +9,30 @@ import pdb
 
 class DappBrowser(SLDapp):
     def initialize(self):
-        self.add_frame(DappBrowserFrame, height=15, width=61, title="Run local Dapp")
+        self.add_frame(DappMenuFrame, height=8, width=50, title="Dapps Menu")
 
-class DappBrowserFrame(SLFrame):
+
+class DappMenuFrame(SLFrame):
     def initialize(self):
-        self.add_label("Dapps directory:")
-        self.add_path_selector(self._select_dir_fn, "Change")
-        self.add_divider(draw_line=True)
+        options = [
+            ("Run network dapp", lambda: self.dapp.add_frame(RunNetworkDappFrame, height=15, width=61, title="Run network Dapp") ),
+            ("Run local dapp", lambda: self.dapp.add_frame(RunLocalDappFrame, height=10, width=50, title="Run local Dapp") ),
+            ("Change local dapp directory", lambda: self.dapp.add_frame(DappDirFrame, height=7, width=75, title="Change Dapp Directory") ),
+            ("Deploy local dapp to network", lambda: self.dapp.add_frame(DeployDappFrame, height=15, width=61, title="Deploy your Dapp") ),
+        ]
+        self._listbox_value = self.add_listbox(4, options, on_select=self._menu_action)
+        self.add_button(self.close, "Cancel")
+
+    def _menu_action(self):
+        self._listbox_value()()
+        self.close()
+
+class RunLocalDappFrame(SLFrame):
+    def initialize(self):
         self.add_label("Your Dapps:")
         options = self._dapps_in_path
         self._listbox_value = self.add_listbox(4, options, on_select=self._run_dapp)
-        self.add_button(self.close, "Cancel", layout_distribution=[80, 20], layout_index=1)
-
-    def _select_dir_fn(self):
-        self.dapp.add_frame(DirPickerFrame, height=21, width=70, title="Choose local Dapp directory")
-
-    def add_path_selector(self, button_fn, text):
-        layout = Layout([80, 20])
-        self.add_layout(layout)
-        layout.add_widget(Label(lambda: self.dapp.config.sl_dapp_path), 0)
-        layout.add_widget(Button(text, button_fn), 1)
-        layout.add_widget(Divider(draw_line=False))
+        self.add_button(self.close, "Cancel")
 
     def _dapps_in_path(self):
         chosen_path = Path(self.dapp.config._sl_dapp_path)
@@ -43,7 +46,6 @@ class DappBrowserFrame(SLFrame):
         if re.search(r'class Dapp', file_text) is not None:
             return True
         return False
-
 
     def reload_package(self, package):
         assert(hasattr(package, "__package__"))
@@ -64,6 +66,7 @@ class DappBrowserFrame(SLFrame):
                             reload_recursive_ex(module_child)
 
         return reload_recursive_ex(package)
+
 
     def _run_dapp(self):
         dapp_name = self._listbox_value()
@@ -88,9 +91,23 @@ class DappBrowserFrame(SLFrame):
             destroy_window=self
         )
 
-        #import traceback
-        #self.dapp.tb_str = traceback.format_exc(limit=10)
-        #self.dapp.add_frame(ErrorFrame, height=23, width=80, title="The Dapp exited with an Exception")
+
+class DappDirFrame(SLFrame):
+    def initialize(self):
+        self.add_label("Dapps directory:")
+        self.add_path_selector(self._select_dir_fn, "Change")
+        self.add_button(self.close, "Cancel")
+
+    def _select_dir_fn(self):
+        self.dapp.add_frame(DirPickerFrame, height=21, width=70, title="Choose local Dapp directory")
+
+    def add_path_selector(self, button_fn, text):
+        layout = Layout([80, 20])
+        self.add_layout(layout)
+        layout.add_widget(Label(lambda: self.dapp.config.sl_dapp_path), 0)
+        layout.add_widget(Button(text, button_fn), 1)
+        layout.add_widget(Divider(draw_line=False))
+
 
 
 class DirPickerFrame(SLFrame):
