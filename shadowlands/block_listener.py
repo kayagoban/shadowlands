50964e7252a2ce094deeca5edf84bcefe72f0bf3
C Thomas <cthomas@soykaf.digital>
2019-05-28 21:24:29 -0500

    cleaner cleanup of txs

--- shadowlands/block_listener.py
+++ shadowlands/block_listener.py
@@ -18,6 +18,9 @@ class BlockListener():
         self.config = config
         self.node = node
         self.shutdown = False
+
+        # send fake event to clean up any malingering txs in queue
+        self.handle_event(None)
         
         self.thread = threading.Thread(target=self.listen, args=([6]))
         #self.listen(6)
@@ -35,63 +38,16 @@ class BlockListener():
         self.shutdown = True
         self.thread.join()
 
-    def remove_mined_txs(self):
-        mined_txs = []
-
-        for tx in self.config.txqueue(self.node.network):
-            fresh_tx = self.node.w3.eth.getTransaction(tx.hash)
-
-            if fresh_tx is None:
-                mined_txs.append(tx)
-            elif fresh_tx.blockNumber is not None:
-                mined_txs.append(tx)
-
-        for tx in mined_txs:
-            self.config.txqueue_remove(self.node.network, tx)
-            logging.info("removing tx {}({})".format(tx.nonce, tx.hash.hex()))
-
-    ''' 
-    This method cleans the txqueue of txs which have been confirmed or which are no longer possible to confirm.
-    new_txs is a list of tx hashes which have been mined.
-    '''
-    def clean_txqueue(self, new_txs):
-        logging.info("block_listener clean_txqueue ({})".format(time.ctime()), poll_interval)
-        intersection = [x for x in self.config.txqueue(self.node.network) if x.hash in new_txs] 
-
-        for match in intersection:
-            nonce = match.nonce
-            address = match["from"]
-
-            # grabs all txs in txqueue with same 'from address' as 
-            # mined txs, and the same nonce or lower.
-            smaller_nonces = [
-                x for x 
-                in self.config.txqueue(self.node.network)
-                if x['from'] == address
-                and x.nonce <= nonce
-            ] 
-
-            for x in smaller_nonces:
-                self.config.txqueue_remove(self.node.network, x)
-                logging.info("removing tx")
-
 
     def handle_event(self, event):
-        txs = [ self.node.w3.eth.getTransaction(x.hash) for x in self.config.txqueue(self.node.network)]
-        logging.debug("examining txs for potential removal: {}".format(txs))
-        for tx in txs:
-            if tx.blockNumber is not None:
-                # remove tx and any txs for this address with lesser nonces.
-                smaller_nonces = [
-                    x for x 
-                    in self.config.txqueue(self.node.network)
-                    if x['from'] == tx['from']
-                    and x.nonce <= tx.nonce
-                ] 
-
-                for y in smaller_nonces:
-                    self.config.txqueue_remove(self.node.network, y)
-                    logging.info("{} mined in block {}".format(y.hash.hex(), y.blockNumber))
+        # dedupe txqueue addresses
+        addresses = set([x['from'] for x in self.config.txqueue(self.node.network)])
+        logging.info("block listener checking addresses: {}".format(addresses))
+        for x in addresses:
+            expired_txs = [y for y in self.config.txqueue(self.node.network) if y['from'] == x and y.nonce <= (self.node.w3.eth.getTransactionCount(x) - 1)]
+            for y in expired_txs:
+                self.config.txqueue_remove(self.node.network, y)
+                logging.info("tx {} expired".format(y.hash.hex()))
 
 
     def listen(self, poll_interval):
