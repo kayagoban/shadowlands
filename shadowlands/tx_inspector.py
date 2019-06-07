ab5eb4caf03059668ae641e8cbdd862d80141034
C Thomas <cthomas@soykaf.digital>
2019-06-07 13:03:22 -0500

    Fix Nuke button and rework look of tx inspector

--- shadowlands/tx_inspector.py
+++ shadowlands/tx_inspector.py
@@ -21,42 +21,64 @@ class TxInspector(SLDapp):
         )
  
     def initialize(self):
-        self.add_sl_frame(TxDetail(self, 22, 71, title='Pending Tx Detail'))
+        #self.add_label(self.dapp.tx.hash.hex(), add_divider=True)
+        self.add_sl_frame(
+            TxDetail(self, 15, 71, title=self.tx.hash.hex().replace('0x',''))
+        )
+        #self.add_sl_frame(TxDetail(self, 18, 71, title='TX detail'))
 
 class TxDetail(SLFrame):
     def initialize(self):
-        self.add_label(self.dapp.tx.hash.hex(), add_divider=False)
-        self.add_button(lambda: self.copy_to_clipboard(self.dapp.tx.hash.hex()), "Copy TxHash", layout_distribution=[70, 30], layout_index=1)
-        #self.add_divider(draw_line=True)
-        self.add_label("Network: {}".format(self.dapp.node.NETWORKDICT[self.dapp.tx['chainId']]))
+        self.add_divider()
+
+        top_buttons = [
+           ("Copy TxHash", lambda: self.copy_to_clipboard(self.dapp.tx.hash.hex()), 2)
+        ]
+
+        if self.dapp.tx['from'] == self.dapp.node.credstick.address:
+            top_buttons.append( ("Resubmit Tx", self.resend_tx, 0))
+            top_buttons.append( ("Nuke Tx", self.nuke_tx, 1))
+ 
+        self.add_button_row(top_buttons, layout=[30, 30, 40], add_divider=False)
+        self.add_divider()
+
+        #self.add_label(self.dapp.tx.hash.hex(), add_divider=True)
+
+        try:
+            nname = self.dapp.node.NETWORKDICT[self.dapp.tx['chainId']]
+        except KeyError:
+            nname = "Unknown"
+
+        #self.add_label_with_button("Network: {}".format(nname), "Copy TxHash", lambda: self.copy_to_clipboard(self.dapp.tx.hash.hex()))
+
+        decimalstr = "{:f}".format(Decimal(self.dapp.tx.gasPrice) / GWEI)
+        #decimalstr ="{:f}".format(Decimal(12.345))
+        self.add_label_row([
+            ("Nonce: {}".format(self.dapp.tx.nonce), 0),
+            ("Gas Price: {} Gwei".format(decimalstr[:6]), 1),
+            ("Network: {}".format(nname), 2)
+            ],
+            layout=[25, 45, 30]
+        )
+        self.add_label("Value: {} ETH".format(Decimal(self.dapp.tx.value) / ETH))
+        #self.add_divider()
+
+        #self.add_label("Gas Price: {} Gwei".format(Decimal(self.dapp.tx.gasPrice) / GWEI ))
+        #self.add_label("Nonce: {}".format(self.dapp.tx.nonce))
+
         # 'from' is a python keyword so we must access the item
         # using bracket notation
-        self.add_label_with_button("From: {}".format(self.dapp.tx['from']), "Copy", lambda: self.copy_to_clipboard(self.dapp.tx['from']))
-        self.add_label_with_button("To: {}".format(self.dapp.tx.to), "Copy", lambda: self.copy_to_clipboard(self.dapp.tx.to))
-        self.add_label("Nonce: {}".format(self.dapp.tx.nonce))
-        self.add_label("Value: {} ETH".format(
-            Decimal(self.dapp.tx.value) / ETH))
-        self.add_label_with_button("Input data: {}".format(self.dapp.tx.input), "Copy", lambda: self.copy_to_clipboard(self.dapp.tx.input))
-        self.add_label("Gas Price: {} Gwei".format(Decimal(self.dapp.tx.gasPrice) / GWEI ))
-        self.add_label("Gas Limit: {} Gwei".format(self.dapp.tx.gas), add_divider=False)
-        #debug(); pdb.set_trace()
-        #self.checksum = self.add_textbox("SHA256:")
-        self.add_divider()
-        self.add_divider()
-        #self.add_divider(draw_line=True)
-        if self.dapp.tx['from'] == self.dapp.node.credstick.address:
-            self.add_button_row([
-                ("Resubmit Tx", self.resend_tx, 0),
-                ("Nuke Tx", self.nuke_tx, 1),
-                ("Back", self.close, 3)
-            ],
-            layout=[25, 25, 35, 18])
-        else:
-            self.add_button(self.close, "Back", layout_distribution=[80, 20], layout_index=1)
+        self.add_label_with_button("From: {}".format(self.dapp.tx['from']), "Copy", lambda: self.copy_to_clipboard(self.dapp.tx['from']), add_divider=False)
+        self.add_label_with_button("To:   {}".format(self.dapp.tx.to), "Copy", lambda: self.copy_to_clipboard(self.dapp.tx.to))
+        self.add_label_with_button("Input data: {}".format(self.dapp.tx.input[:12]), "Copy", lambda: self.copy_to_clipboard(self.dapp.tx.input))
+        #self.add_label("Gas Limit: {} Gwei".format(self.dapp.tx.gas), add_divider=False)
+        self.add_label_with_button("Gas Limit: {} Gwei".format(self.dapp.tx.gas), "Back", self.close, add_divider=False)
+        #if self.dapp.tx['from'] == self.dapp.node.credstick.address:
+       ##else:
+        #    self.add_button(self.close, "Back", layout_distribution=[80, 20], layout_index=1)
 
     # 0 eth tx from and to current address
     def nuke_tx(self):
-        #debug(); pdb.set_trace()
         tx_dict = self.dapp.node.build_send_tx(
             0,
             self.dapp.node.credstick.address,
@@ -64,7 +86,7 @@ class TxDetail(SLFrame):
             nonce=self.dapp.tx.nonce
         )
 
-        self.dapp.add_send_dialog(
+        self.dapp.add_resend_dialog(
             tx_dict,
             "Overwrite nonce {} Tx with No-Op".format(self.dapp.tx.nonce)
         )
